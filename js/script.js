945d677d14d9fa1bc0816cae1cd8755d28298552
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-02-15 17:23:32 +0100

    sezione 2,3,4

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 var app = new Vue({
     el: '#root',
     data: {
-        listaHeaderSx: ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop']
+        listaHeaderSx: ['Home', 'Pages', 'Courses', 'Features', 'Blog', 'Shop'],
+        corsi: [
+            {
+                immagine: '1',
+                costo: '$40.00',
+                descrizione: 'Learning to write as a Professional Author'
+            },
+            {
+                immagine: '2',
+                costo: '$0.00',
+                descrizione: 'Customer-centric Info-Tech Strategies'
+            },
+            {
+                immagine: '3',
+                costo: '$19.00',
+                descrizione: 'IOpening Programming for Everyone: Python'
+            }
+        ]
     }
 });
