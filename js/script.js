eaf1474977247f611795e607e0a76128170b6d04
Ilaria Mammucari <ilariamammucari@hotmail.it>
2021-02-16 11:28:53 +0100

    da finire

--- js/script.js
+++ js/script.js
@@ -6,34 +6,46 @@ var app = new Vue({
             {
                 immagine: '1',
                 costo: '$40.00',
-                descrizione: 'Learning to write as a Professional Author'
+                descrizione: 'Learning to write as a Professional Author',
+                lezioni: '20 Lessons',
+                studenti: '50 Students'
             },
             {
                 immagine: '2',
                 costo: '$0.00',
-                descrizione: 'Customer-centric Info-Tech Strategies'
+                descrizione: 'Customer-centric Info-Tech Strategies',
+                lezioni: '24 Lessons',
+                studenti: '769 Students'
             },
             {
                 immagine: '3',
                 costo: '$19.00',
-                descrizione: 'Open Programming for Everyone: Python'
+                descrizione: 'Open Programming for Everyone: Python',
+                lezioni: '17 Lessons',
+                studenti: '62 Students'
             }
         ],
         blogs: [
             {
                 immagine: '1.jpeg',
                 tipo: 'ARTIST',
-                titolo: 'Brush Strokes Energize Trees in Paintings'
+                titolo: 'Brush Strokes Energize Trees in Paintings',
+                calendario: 'May 15, 2020',
+                visualizzazioni: '688 views'
             },
             {
                 immagine: '2.jpg',
                 tipo: 'ARTIST',
-                titolo: 'Pocket-Sized Notebooks Hold Miniature paitings'
+                titolo: 'Pocket-Sized Notebooks Hold Miniature paitings',
+                calendario: 'May 15, 2020',
+                visualizzazioni: '688 views'
             },
             {
                 immagine: '3.jpg',
                 tipo: 'ARTIST',
-                titolo: 'Connection Between Self-Portraits and Identity'
+                titolo: 'Connection Between Self-Portraits and Identity',
+                calendario: 'May 15, 2020',
+                visualizzazioni: '688 views'
             }
         ],
         indirizzi: ['328 Ne 19lst St # 87394 Miami, FL 33270-3899', '+1 (305) 547-9909 (9am - 5pm EST, Monday -Friday)', 'support@maxcoach.com'],
